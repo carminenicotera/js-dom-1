ada8650715d112fb0186e6ac34f3f0888a98fcfc
carminenicotera <carmine.91.n@hotmail.it>
2026-02-11 16:23:38 +0100

    add eventListener

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//raccolta dati
+const whiteLampEl = document.getElementById('off');
+const btnEl = document.getElementById('btn');
+
+//aggiungo evento per far si che al click venga sostituita l'immagine della lampadina spenta con quella accesa
+btnEl.addEventListener('click', function(){
+  console.log('stai cliccando');
+  whiteLampEl.src = "../img/yellow_lamp.png";
+  
+})
